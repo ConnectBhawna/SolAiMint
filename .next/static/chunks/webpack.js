935bb0b6e89f4e8a5f6d7472520d34248c988bea
bhawna <bhawnabc2002@gmail.com>
2022-11-19 12:40:00 +0530

    logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1b397949e19e6922"; }
+/******/ 		__webpack_require__.h = function() { return "67539c37ae9b74c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -6,8 +6,7 @@ import Image from 'next/image'
 export const AppBar: FC = () => {
     return (
         <div className={styles.AppHeader}>
-            <Image src="/solanaLogo.png" height={30} width={200} />
-            <span>Wallet-Adapter Example</span>
+            <Image src="/logo.png" height={100} width={200} />
             <WalletMultiButton />
         </div>
     )
